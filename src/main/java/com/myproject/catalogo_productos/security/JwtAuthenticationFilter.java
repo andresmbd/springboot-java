77e31d1f1c8be803f926cba10dcddae85945221c
hs15yg <hilayepes@gmail.com>
2026-09-17 20:39:46 -0500

    bug

--- src/main/java/com/myproject/catalogo_productos/security/JwtAuthenticationFilter.java
+++ src/main/java/com/myproject/catalogo_productos/security/JwtAuthenticationFilter.java
@@ -28,24 +28,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");
+
+        System.out.println(">>> JWT FILTER");
+        System.out.println(">>> METHOD: " + request.getMethod());
+        System.out.println(">>> URL: " + request.getRequestURI());
+        System.out.println(">>> Authorization: " + header);
+
         if (header == null || !header.startsWith("Bearer ")) {
+            System.out.println(">>> NO HAY BEARER TOKEN");
             filterChain.doFilter(request, response);
             return;
         }
 
         String token = header.substring("Bearer ".length());
+
         try {
             String username = jwtService.extractUsername(token);
+
+            System.out.println(">>> USERNAME DEL TOKEN: " + username);
+
             if (SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails user = userDetailsService.loadUserByUsername(username);
+
+                System.out.println(">>> USUARIO ENCONTRADO: " + user.getUsername());
+
                 if (jwtService.isValid(token, user)) {
+                    System.out.println(">>> JWT VALIDO");
                     var authentication = new UsernamePasswordAuthenticationToken(
                             user, null, user.getAuthorities());
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
+                    System.out.println(">>> AUTENTICACION ESTABLECIDA");
+                }else {
+                    System.out.println(">>> JWT INVALIDO");
                 }
             }
-        } catch (RuntimeException e) {
+        } catch (RuntimeException e) { // catch (RuntimeException ignored) {}
+            System.out.println(">>> ERROR PROCESANDO JWT");
             e.printStackTrace();
         }
         filterChain.doFilter(request, response);
